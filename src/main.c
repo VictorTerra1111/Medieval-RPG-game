816fdb7d683a3302a290d128824652405ac45385
Victor Terra <victorterra221@gmail.com>
2026-06-25 15:38:18 -0300

    made save files 90 percent working

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "raylib.h"
 
 typedef enum {
@@ -6,6 +7,7 @@ typedef enum {
     GAME_MENU,
     GAME_PLAYING,
     GAME_SAVES,
+    GAME_SAVE_ALREADY_EXIST,
     GAME_OPTIONS,
     GAME_EXIT
 } State;
@@ -19,6 +21,55 @@ double fade_rate = 0.0f;
 #define FADE_OUT 1
 #define MENU_FONT_SIZE 40
 #define SCALE 4.0f
+
+typedef enum {
+    SWORD_NONE,
+    SWORD_BURNT,
+    SWORD_WOODEN,
+    SWORD_METAL,
+    SWORD_FALSE_TWILIGHT,
+    SWORD_REAL_TWILIGHT
+} SwordType;
+
+typedef enum {
+    SHIELD_NONE,
+    SHIELD_BURNT,
+    SHIELD_WOODEN,
+    SHIELD_METAL,
+    SHIELD_LIFE
+} ShieldType;
+
+typedef enum {
+    JEWEL_FOREST,
+    JEWEL_TIDE,
+    JEWEL_PASSION,
+    JEWEL_WIND,
+    JEWEL_THUNDER,
+    JEWEL_COUNT
+} Jewel;
+
+typedef struct{
+	char name[32];
+	
+	int life;
+	int mana;
+	
+	SwordType sword; // 0 for no sword, 1 for burnt sword, 2 for wooden, 3 for metal, 4 for false twilight, 5 for real tw
+	ShieldType shield; // 0 for no shield, 1 for burnt shield, 2 for wooden, 3 for metal, 4 for life shield
+	
+	int arrows; // how many arrows, -1 if no bow 
+	bool jewels[5]; // forest, tide, passion, wind, thunder
+	
+	bool Inventory[6]; // for armor: tiara, necklace, earrings, wrist, ring, boots
+	
+	int money; 
+	int collectables;
+	int bells;
+	
+	bool has_whistle;
+	bool can_teleport;
+} MainChar;
+
 
 int fade_screen(int io){
 	
@@ -45,6 +96,124 @@ int fade_screen(int io){
 }
 
 
+int create_save_file(int save_slot){
+	char filename[32];
+	snprintf(filename, sizeof(filename), "save_files/save_file_%d.sav", save_slot);
+
+	FILE *save = fopen(filename, "r");
+	
+	if (save != NULL){
+		fclose(save);
+		return -2; // there is another save in the slot
+	}
+
+	MainChar player = {0};
+
+    	strcpy(player.name, "Renna Legot");
+    	player.life = 100;
+
+    	save = fopen(filename, "wb");
+
+    	if (save == NULL) return -1; // cannot create file
+
+    	fwrite( &player, sizeof(MainChar), 1,save);
+
+    	fclose(save);
+
+    return 0; // ok
+}
+
+
+int delete_save_file(int save_slot) {
+	char filename[32];
+	snprintf(filename, sizeof(filename), "save_files/save_file_%d.sav", save_slot);
+
+    	FILE *save = fopen(filename, "rb");
+
+    	if (save == NULL) return -2; // not found
+
+    	fclose(save);
+
+	if (remove(filename) != 0) return -1; // couldnt erase
+
+    	return 0; // ok
+}
+
+int save_game(MainChar *player, int save_slot){
+    char filename[64];
+
+    snprintf(
+        filename,
+        sizeof(filename),
+        "saves/save_file_%d.sav",
+        save_slot
+    );
+
+    FILE *save = fopen(filename, "wb");
+
+    if (save == NULL)
+        return -1;
+
+    fwrite(
+        player,
+        sizeof(MainChar),
+        1,
+        save
+    );
+
+    fclose(save);
+
+    return 0;
+}
+
+MainChar load_save_file(int save_slot)
+{
+    MainChar player = {0};
+
+    char filename[64];
+
+    snprintf(
+        filename,
+        sizeof(filename),
+        "saves/save_file_%d.sav",
+        save_slot
+    );
+
+    FILE *save = fopen(filename, "rb");
+
+    if (save == NULL)
+        return player;
+
+    if (fread(&player, sizeof(MainChar), 1, save) != 1){
+    	fclose(save);
+    	return player;
+    }
+    
+    fclose(save);
+    return player;
+}
+
+int save_exists(int save_slot)
+{
+    char filename[64];
+
+    snprintf(
+        filename,
+        sizeof(filename),
+        "save_files/save_file_%d.sav",
+        save_slot
+    );
+
+    FILE *save = fopen(filename, "rb");
+
+    if (save == NULL)
+        return 0;
+
+    fclose(save);
+
+    return 1;
+}
+
 int main(void){
 
 	SetTraceLogLevel(LOG_NONE);
@@ -68,6 +237,20 @@ int main(void){
 	int sel2 = 0;
 	int sel = 0;
 	int sel3 = 0;
+	int sel_slot = 1;
+
+	const char *sure = "ARE YOU SURE?";
+                                const char *no = "NO";
+                                const char *yes = "YES";
+
+                                int sure_wid = MeasureText(sure, MENU_FONT_SIZE);
+                                int no_wid = MeasureText(no, MENU_FONT_SIZE);
+                                int yes_wid = MeasureText(yes, MENU_FONT_SIZE);
+
+                                int x_sure = (GetScreenWidth() - sure_wid) / 2;
+                                int x_no = (GetScreenWidth() - no_wid) / 2;
+                                int x_yes = (GetScreenWidth() - yes_wid) / 2;
+
 	while (!WindowShouldClose() && running) {
         	BeginDrawing();
 		ClearBackground(BLACK);
@@ -198,19 +381,45 @@ int main(void){
 			break;
 
 			case GAME_SAVES:
-				ClearBackground(PURPLE);
-				const char *save_file = "EMPTY SLOT";
+				ClearBackground(WHITE);
                                 const char *go_back = "BACK TO START";
-                                int sav_wid= MeasureText(save_file, MENU_FONT_SIZE);
                                 int gob_wid = MeasureText(go_back, MENU_FONT_SIZE);
-                    
-                                int x_sav = (GetScreenWidth() - sav_wid) / 2;
                                 int x_gob = (GetScreenWidth() - gob_wid) / 2;
-                           
+                           	char slot1_text[32] = "EMPTY SLOT";
+				char slot2_text[32] = "EMPTY SLOT";
+				char slot3_text[32] = "EMPTY SLOT";
+			
 
-                                DrawText(save_file, x_sav, 300, 40, BLACK);
-                                DrawText(save_file, x_sav, 400, 40, BLACK);
-                                DrawText(save_file, x_sav, 500, 40, BLACK);
+if(save_exists(1))
+{
+    MainChar p = load_save_file(1);
+    snprintf(slot1_text, sizeof(slot1_text),"%s", p.name);
+                                
+
+}
+
+if(save_exists(2))
+{
+    MainChar p = load_save_file(2);
+    snprintf(slot2_text, sizeof(slot2_text),"%s", p.name);
+}
+
+if(save_exists(3))
+{
+    MainChar p = load_save_file(3);
+    snprintf(slot3_text, sizeof(slot3_text), "%s", p.name);
+}
+				int wid1 = MeasureText(slot1_text, MENU_FONT_SIZE);
+                                int wid2 = MeasureText(slot2_text, MENU_FONT_SIZE);
+                                int wid3 = MeasureText(slot3_text, MENU_FONT_SIZE);
+                                int x_s1 = (GetScreenWidth() - wid1) / 2;
+                                int x_s2 = (GetScreenWidth() - wid2) / 2;
+                                int x_s3 = (GetScreenWidth() - wid3) / 2;
+
+
+                                DrawText(slot1_text, x_s1, 300, 40, BLACK);
+                                DrawText(slot2_text, x_s2, 400, 40, BLACK);
+                                DrawText(slot3_text, x_s3, 500, 40, BLACK);
 				DrawText(go_back, x_gob, 800, 40, BLACK);
 			
 				if (IsKeyPressed(KEY_S)) {
@@ -223,14 +432,45 @@ int main(void){
                                 }
                                 switch(sel3){
                                         case 0:
-                                                DrawText(">", x_sav - 100, 300, 40, BLACK);
-                                                break;
+                                                DrawText(">", x_s1 - 100, 300, 40, BLACK);
+                   				
+						if(IsKeyPressed(KEY_ENTER)){
+							sel_slot = 1;
+							if(create_save_file(1) == -2) current = GAME_PLAYING;
+						}
+						else if(IsKeyPressed(KEY_DELETE)){
+							sel_slot = 1;
+							current = GAME_SAVE_ALREADY_EXIST;
+						}
+						
+						break;
                                         case 1:
-                                                DrawText(">", x_sav - 100, 400, 40, BLACK);
-                                                break;
+                                                DrawText(">", x_s2 - 100, 400, 40, BLACK);
+                                                if(IsKeyPressed(KEY_ENTER)){
+                                               		sel_slot = 2;
+					       		if(create_save_file(2) == -2) current = GAME_PLAYING;
+                                                }
+                                                else if(IsKeyPressed(KEY_DELETE)){
+                                                        sel_slot = 2;
+							current = GAME_SAVE_ALREADY_EXIST;
+                                                }
+
+						break;
                                         case 2:
-                                                DrawText(">", x_sav - 100, 500, 40, BLACK);
-                                                break;
+						
+                                                DrawText(">", x_s3 - 100, 500, 40, BLACK);
+                                                if(IsKeyPressed(KEY_ENTER)){
+                                                	sel_slot = 3;
+							if(create_save_file(3) == -2) current = GAME_PLAYING;
+                                                }
+                                                else if(IsKeyPressed(KEY_DELETE)){
+                                                        sel_slot = 3;
+							sel2 = 0;
+							current = GAME_SAVE_ALREADY_EXIST;
+                                                	
+						}
+
+						break;
 					case 3:
 						DrawText(">", x_gob - 100, 800, 40, BLACK);
 						if(IsKeyPressed(KEY_ENTER)) {
@@ -241,7 +481,37 @@ int main(void){
                                 }
 
 				break;
-    			
+			case GAME_SAVE_ALREADY_EXIST:
+				
+                                ClearBackground(WHITE);
+
+                                if (IsKeyPressed(KEY_D)) {
+                                        if(sel2 < 1) sel2 ++;
+                                        else sel2 = 0;
+                                }
+                                else if (IsKeyPressed(KEY_A)){
+                                        if(sel2 == 0) sel2 = 1;
+                                        else sel2 --;
+                                }
+
+                                DrawText(sure, x_sure, 400, 40, BLACK);
+                                DrawText(no, x_no + 100, 600, 40, BLACK);
+                                DrawText(yes, x_yes - 100, 600, 40, BLACK);
+
+                                if(!sel2) {
+                                        DrawText(">", x_yes - 125, 600, 40, BLACK);
+                                        if(IsKeyPressed(KEY_ENTER)) {
+                                                delete_save_file(sel_slot);
+                                        }
+                                }
+                                else {
+                                        DrawText(">", x_no + 75, 600, 40, BLACK);
+                                        if(IsKeyPressed(KEY_ENTER)) current = GAME_SAVES;
+                                }
+
+				
+			break;
+			
 			case GAME_PLAYING:
 				ClearBackground(GREEN);
 				// printf("ENTERED GAME_PLAYING STATE\n");
@@ -257,18 +527,6 @@ int main(void){
 			break;
 
 			case GAME_EXIT:
-				const char *sure = "ARE YOU SURE?";
-				const char *no = "NO";
-				const char *yes = "YES";
-
-				int sure_wid = MeasureText(sure, MENU_FONT_SIZE);
-				int no_wid = MeasureText(no, MENU_FONT_SIZE);
-				int yes_wid = MeasureText(yes, MENU_FONT_SIZE);
-
-				int x_sure = (GetScreenWidth() - sure_wid) / 2;
-				int x_no = (GetScreenWidth() - no_wid) / 2;
-				int x_yes = (GetScreenWidth() - yes_wid) / 2;
-			       	
 				ClearBackground(WHITE);
 
 				if (IsKeyPressed(KEY_D)) {
